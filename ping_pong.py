5a43aa1e77025e236e05c5b22debd60a6209432f
Попов Михаил <122226424+MikhailKamado@users.noreply.github.com>
2023-05-22 16:27:38 +0300

    Update ping_pong.py
    Добавлен класс Player. Теперь в игре присутствуют ракетки, которые управляются стрелочками и WSAD

--- ping_pong.py
+++ ping_pong.py
@@ -11,6 +11,19 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
 back = (200, 255, 255) 
 win_width = 600
 win_height = 500
@@ -20,6 +33,8 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60    
+racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 font.init()
 font = font.Font(None, 35)
@@ -34,8 +49,13 @@ while game:
             game = False
     if finish != True:
         window.fill(back)
+        racket1.update_l()
+        racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
         if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
         if ball.rect.x < 0:
@@ -46,6 +66,8 @@ while game:
             finish = True
             window.blit(lose2, (200, 200))
             game_over = True
+        racket1.reset()
+        racket2.reset()
         ball.reset()
     display.update()
     clock.tick(FPS)
